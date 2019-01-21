0c421d7ba02494c8e7a930300c664401dfff9434
Ankon <44019225+luciferankon@users.noreply.github.com>
2019-01-21 09:30:36 +0530

    Added documentation

--- src/express.js
+++ src/express.js
@@ -1,3 +1,9 @@
+/**
+ * Checks request and route are same or not
+ * @param {Object} req - requested URL
+ * @param {Object} route - one of the routes to the server
+ */
+
 const isMatching = (req, route) => {
   if (route.method && req.method != route.method) return false;
   if (route.url instanceof RegExp && route.url.test(req.url)) return true;
@@ -6,14 +12,21 @@ const isMatching = (req, route) => {
 };
 
 class Express {
+  /**
+   * @constructor
+   */
   constructor() {
     this.routes = [];
   }
-
+  /**
+   * request coming to the server
+   * @param {Object} req - request from the client
+   * @param {Object} res - response given from the server
+   */
   handleRequest(req, res) {
     let matchingRoutes = this.routes.filter(r => isMatching(req, r));
     let remaining = [...matchingRoutes];
-    
+
     let next = () => {
       let current = remaining[0];
       if (!current) return;
@@ -22,18 +35,29 @@ class Express {
     };
     next();
   }
-
+  /**
+   * push the handler in the routes
+   * @param {Callback} handler handler for the request
+   */
   use(handler) {
     this.routes.push({ handler });
   }
-
+  /**
+   * push the path in the routes with the handler and GET method
+   * @param {String} url route to the server for post method
+   * @param {Callback} handler handler for the request
+   */
   get(url, handler) {
     this.routes.push({ method: "GET", url, handler });
   }
-
+  /**
+   * push the path in the routes with the handler and POST method
+   * @param {String} url route to the server for post method
+   * @param {Callback} handler handler for the request
+   */
   post(url, handler) {
     this.routes.push({ method: "POST", url, handler });
   }
 }
-
+/**  Exports the Express class */
 module.exports = Express;
